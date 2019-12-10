af5f22a79977697e0704a79b4addd95e1953d5b9
HH <jwalag87@gmail.com>
2019-12-10 14:09:06 +0900

    config수정

---
+++ frontend/assets/config.js
@@ -0,0 +1,8 @@
+export const meetingTypeMap = new Map()
+meetingTypeMap.set('plenary_session', '본회의')
+
+export const URL = ''
+
+export const searchTypeMap = new Map()
+searchTypeMap.set('agenda', '안건')
+searchTypeMap.set('discussion', '발언')
