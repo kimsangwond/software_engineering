533db0036d54eb58a756d32cb21fac9cf43ee15f
HH <jwalag87@gmail.com>
2019-12-14 22:55:28 +0900

    구독 중인 국회의원 없을 시에 알려주도록

--- frontend/pages/SubscriberListView.vue
+++ frontend/pages/SubscriberListView.vue
@@ -1,40 +1,45 @@
 <template>
-    <div class="mt-4 col-sm-10 offset-sm-1">
-      <b-card header="구독중인 국회의원 명단">
-        <b-card-group deck>
-          <b-card 
-            v-for="congressMember in subcribedCongressMemberList" 
-            :key="congressMember.name" 
-            :header="congressMember.party"
-            bg-variant="light" 
-            class="text-center"
-            style="max-width: 20rem"
-          >
-              <b-card-text>
-                {{ congressMember.name }}
-              </b-card-text>
-              <b-button 
-                @click="searchByName(congressMember.name, 'agenda')"
-                variant="success"
-                >
-                안건 검색
-              </b-button>
-              <b-button
-                @click="searchByName(congressMember.name, 'discussion')"
-                variant="success"
-                >
-                발언 검색
-              </b-button>
-              <b-button 
-                @click="cancleSubscribe(congressMember.name)" 
-                variant="danger"
-                >
-                구독 취소
-              </b-button>
-          </b-card>
-        </b-card-group>
-      </b-card>
-    </div>
+  <div class="mt-4 col-sm-10 offset-sm-1">
+    <b-card header="구독중인 국회의원 명단" v-if="subcribedCongressMemberList">
+      <b-card-group deck>
+        <b-card 
+          v-for="congressMember in subcribedCongressMemberList" 
+          :key="congressMember.name" 
+          :header="congressMember.party"
+          bg-variant="light" 
+          class="text-center"
+          style="max-width: 20rem"
+        >
+            <b-card-text>
+              {{ congressMember.name }}
+            </b-card-text>
+            <b-button 
+              @click="searchByName(congressMember.name, 'agenda')"
+              variant="success"
+              >
+              안건 검색
+            </b-button>
+            <b-button
+              @click="searchByName(congressMember.name, 'discussion')"
+              variant="success"
+              >
+              발언 검색
+            </b-button>
+            <b-button 
+              @click="cancleSubscribe(congressMember.name)" 
+              variant="danger"
+              >
+              구독 취소
+            </b-button>
+        </b-card>
+      </b-card-group>
+    </b-card>
+    <b-card header="구독중인 국회의원 명단" v-else>
+      <b-card-text>
+        현재 구독 중인 국회의원이 없습니다.
+      </b-card-text>
+    </b-card>
+  </div>
 </template>
 
 <script>
