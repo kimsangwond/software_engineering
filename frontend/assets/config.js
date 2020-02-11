fe227721de8b5cdc103b89f0c496e0dc8bf4d868
kimsangwon <tkddnjs2014@naver.com>
2020-02-11 15:40:34 +0900

    latex update7

--- frontend/assets/config.js
+++ frontend/assets/config.js
@@ -1,3 +1,6 @@
 export const meetingTypeMap = new Map()
 meetingTypeMap.set('plenary_session', '본회의')
-export const URL = 'https://bbee9a4a.ngrok.io/politic'
+export const URL = 'http://af6ba55f.ngrok.io/politic'
+export const searchTypeMap = new Map()
+searchTypeMap.set('agenda', '안건')
+searchTypeMap.set('discussion', '발언')
